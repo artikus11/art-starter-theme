e3f970075b42f77eeaa4a67d0d2d9a6894eff074
Artem Abramovich <artikus.sol@gmail.com>
2017-06-07 15:38:59 +0400

    add new file enqueue scripts and styles

---
+++ includes/enqueue-scripts-style.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Enqueue all styles and scripts
+ *
+ */
+if ( ! function_exists( 'ast_scripts' ) ) :
+	add_action( 'wp_enqueue_scripts', 'ast_scripts' );
+	function ast_scripts() {
+		
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+	}
+
+endif;
+if ( ! function_exists( 'ast_styles' ) ) :
+	add_action( 'wp_enqueue_scripts', 'ast_styles' );
+	function ast_styles() {
+		wp_enqueue_style( 'ast-style', get_stylesheet_uri() );
+	}
+
+endif;
