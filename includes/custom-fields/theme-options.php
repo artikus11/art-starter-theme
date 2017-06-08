808458cc4488664383ab461a53b877aef463de38
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 19:32:22 +0400

    add theme options test

--- includes/custom-fields/theme-options.php
+++ includes/custom-fields/theme-options.php
@@ -5,3 +5,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
+
+// Default options page
+Container::make('theme_options', 'Basic Options')
+         ->add_fields(array(
+	         Field::make('header_scripts', 'crb_header_script'),
+	         Field::make('footer_scripts', 'crb_footer_script'),
+         ));
+
+// Add second options page under 'Basic Options'
+Container::make('theme_options', 'Social Links')
+         ->set_page_parent('Basic Options')  // title of a top level Theme Options page
+         ->add_fields(array(
+		Field::make('text', 'crb_facebook_link'),
+		Field::make('text', 'crb_twitter_link')
+	));
+
+// Add third options page under "Appearance"
+Container::make('theme_options', 'Customize Background')
+         ->set_page_parent('themes.php') // identificator of the "Appearance" admin section
+         ->add_fields(array(
+		Field::make('color', 'crb_background_color'),
+		Field::make('image', 'crb_background_image')
+	));
