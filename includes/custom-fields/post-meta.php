58bd8701c1ecf2c67db16dd44ad712d2b40dc067
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 20:46:57 +0400

    add page filed test

--- includes/custom-fields/post-meta.php
+++ includes/custom-fields/post-meta.php
@@ -5,3 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
+
+Container::make('post_meta', 'Custom Data')
+         ->show_on_post_type('page')
+         ->add_fields(array(
+	         Field::make('map', 'crb_location')->set_position(37.423156, -122.084917, 14),
+	         Field::make('choose_sidebar', 'crb_custom_sidebar'),
+	         Field::make('image', 'crb_photo'),
+         ));
