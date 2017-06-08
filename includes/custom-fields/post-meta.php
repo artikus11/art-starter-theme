365604b4b3381b38a860153ae7ad12bc00a512e6
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 21:20:37 +0400

    fix errore

--- includes/custom-fields/post-meta.php
+++ includes/custom-fields/post-meta.php
@@ -6,7 +6,7 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 Container::make( 'post_meta', 'Custom Data' )
-	->show_on_template( 'main-page.php' )
+	->show_on_template( 'templates/main-page.php' )
 	->add_fields( array(
 		Field::make( 'image', 'crb_photo' ),
 	) );
