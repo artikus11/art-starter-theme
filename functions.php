d5603c038af5733ce371496ec8ae2799c7b342dc
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 21:04:22 +0400

    add load file to functions.php

--- functions.php
+++ functions.php
@@ -1,20 +1,20 @@
 <?php
 /** Add theme support */
-require get_template_directory() .'includes/theme-support.php';
+require get_template_directory() . '/includes/theme-support.php';
 /** Enqueue scripts */
-require get_template_directory() . 'includes/enqueue-scripts-style.php';
+require get_template_directory() . '/includes/enqueue-scripts-style.php';
 /** Various clean up functions */
-require get_template_directory() . 'includes/cleanup.php';
+require get_template_directory() . '/includes/cleanup.php';
 /** Return entry meta information for posts */
-require get_template_directory() . 'includes/meta-data.php';
+require get_template_directory() . '/includes/meta-data.php';
 /** Create widget areas in sidebar and footer */
-require get_template_directory() . 'includes/widget-areas.php';
+require get_template_directory() . '/includes/widget-areas.php';
 /** Add register nav menu */
-require get_template_directory() . 'includes/navigation.php';
+require get_template_directory() . '/includes/navigation.php';
 /** Add Carbon Fields */
-require get_template_directory() . 'includes/carbon-fields/carbon-fields-plugin.php';
+require get_template_directory() . '/includes/carbon-fields/carbon-fields-plugin.php';
 add_action( 'carbon_register_fields', 'ast_register_custom_fields' );
 function ast_register_custom_fields() {
-	require get_template_directory() .'/includes/custom-fields/post-meta.php';
+	require get_template_directory() . '/includes/custom-fields/post-meta.php';
 	require get_template_directory() . '/includes/custom-fields/theme-options.php';
 }
