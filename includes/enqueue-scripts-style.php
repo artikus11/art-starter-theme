b0461afac839fefb226ed5a7d5131fb0c7a27825
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 14:43:59 +0400

    enqueue script magnific & owl

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -5,12 +5,14 @@
 if ( ! function_exists( 'ast_scripts' ) ) :
 	add_action( 'wp_enqueue_scripts', 'ast_scripts' );
 	function ast_scripts() {
-		
+		wp_enqueue_script( 'magnific-popup', get_template_directory_uri() .
+		                                     '/assets/js/jquery.magnific-popup.min.js', array( 'jquery' ), nuul, true );
+		wp_enqueue_script( 'owl-script', get_template_directory_uri() .
+		                                 '/assets/js/owl.carousel.min.js', array( 'jquery' ), nuul, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 	}
-
 endif;
 /**
  * Enqueue all scripts
@@ -20,5 +22,4 @@ if ( ! function_exists( 'ast_styles' ) ) :
 	function ast_styles() {
 		wp_enqueue_style( 'ast-style', get_stylesheet_uri() );
 	}
-
 endif;
