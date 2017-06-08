9af22d85966f024e10a6e7f485c0504dce8dc877
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 13:18:32 +0400

    add new file widget areas

--- includes/widget-areas.php
+++ includes/widget-areas.php
@@ -1,7 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Артем
- * Date: 08.06.2017
- * Time: 12:37
+ * Register widget areas
  */
+if ( ! function_exists( 'ast_sidebar_widgets' ) ) {
+	add_action( 'widgets_init', 'ast_sidebar_widgets' );
+	function ast_sidebar_widgets() {
+		register_sidebar( array(
+			'id'            => 'sidebar-widgets',
+			'name'          => __( 'Sidebar widgets', 'art-starter-theme' ),
+			'description'   => __( 'Drag widgets to this sidebar container.', 'art-starter-theme' ),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<div>',
+			'after_title'   => '</div>',
+		) );
+		register_sidebar( array(
+			'id'            => 'footer-widgets',
+			'name'          => __( 'Footer widgets', 'art-starter-theme' ),
+			'description'   => __( 'Drag widgets to this footer container', 'art-starter-theme' ),
+			'before_widget' => '<aside id="%1$s" class="large-4 columns widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<div>',
+			'after_title'   => '</div>',
+		) );
+	}
+}
