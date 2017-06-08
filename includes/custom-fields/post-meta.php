867bafb49df43973bcb8595f87e7e5b60a0aeb11
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 21:09:41 +0400

    add main page field

--- includes/custom-fields/post-meta.php
+++ includes/custom-fields/post-meta.php
@@ -2,14 +2,11 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make('post_meta', 'Custom Data')
-         ->show_on_post_type('page')
-         ->add_fields(array(
-	         Field::make('map', 'crb_location')->set_position(37.423156, -122.084917, 14),
-	         Field::make('choose_sidebar', 'crb_custom_sidebar'),
-	         Field::make('image', 'crb_photo'),
-         ));
+Container::make( 'post_meta', 'Custom Data' )
+	->show_on_template( 'main-page.php' )
+	->add_fields( array(
+		Field::make( 'image', 'crb_photo' ),
+	) );
