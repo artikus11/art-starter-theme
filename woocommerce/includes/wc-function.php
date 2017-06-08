9cf9d945e5a81c6d5e2d1d4b39c51092530388d2
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 14:49:05 +0400

    add function for WC

---
+++ woocommerce/includes/wc-function.php
@@ -0,0 +1,16 @@
+<?php
+//Хлебные крошки
+if ( ! function_exists( 'ast_breadrumbs_yoast' ) ) {
+	add_action( 'woocommerce_before_main_content', 'ast_breadrumbs_yoast', 10 );
+	function ast_breadrumbs_yoast() {
+		if ( is_product() || is_product_category() || is_shop() ) {
+			if ( function_exists( 'yoast_breadcrumb' ) ) {
+				$before      = '<div id="breadcrumbs">';
+				$after       = '</div>';
+				$breadcrumbs = yoast_breadcrumb( $before, $after, true );
+				
+				return $breadcrumbs;
+			}
+		}
+	}
+}
--- woocommerce/includes/wc-remove-functions.php
+++ woocommerce/includes/wc-remove-functions.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 /*remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
