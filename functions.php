b95b0b976a6c7628919841b91a641822aef0d5f2
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 16:10:15 +0400

    update functions.php

--- functions.php
+++ functions.php
@@ -9,15 +9,12 @@ require_once( 'includes/cleanup.php' );
 require_once( 'includes/meta-data.php' );
 /** Create widget areas in sidebar and footer */
 require_once( 'includes/widget-areas.php' );
-/** Return entry meta information for posts */
-require_once( 'includes/entry-meta.php' );
-
+/** Add register nav menu */
+require_once( 'includes/navigation.php' );
 /** Add Carbon Fields */
 require_once( 'includes/carbon-fields/carbon-fields-plugin.php' );
-
-
-add_action('carbon_register_fields', 'ast_register_custom_fields');
+add_action( 'carbon_register_fields', 'ast_register_custom_fields' );
 function ast_register_custom_fields() {
-	include_once(dirname(__FILE__) . '/includes/custom-fields/post-meta.php');
-	include_once(dirname(__FILE__) . '/includes/custom-fields/theme-options.php');
+	include_once( dirname( __FILE__ ) . '/includes/custom-fields/post-meta.php' );
+	include_once( dirname( __FILE__ ) . '/includes/custom-fields/theme-options.php' );
 }
