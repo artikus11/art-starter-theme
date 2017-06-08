534f7ff4290fff12dac65c6c478f3f4f6a2f5a23
Artem Abramovich <artikus.sol@gmail.com>
2017-06-08 21:54:11 +0400

    update master

--- includes/custom-fields/post-meta.php
+++ includes/custom-fields/post-meta.php
@@ -5,9 +5,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-/*Container::make( 'post_meta', 'Custom Data' )
+Container::make( 'post_meta', 'Custom Data' )
 	->show_on_post_type('page')
 	->show_on_template( 'templates/main-page.php' )
 	->add_fields( array(
 		Field::make( 'image', 'crb_photo' ),
-	) );*/
+	) );
